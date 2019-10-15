26cbe572245c3b65fdfc5ce4b6976e6a7aa27f38
Aime1007 <1310959322@qq.com>
2019-10-15 17:36:27 +0800

    Update main.cpp

--- 0910/0910/main.cpp
+++ 0910/0910/main.cpp
@@ -1,14 +1,43 @@
 #include<stdio.h>
+#include<stdlib.h>
+#define ERROR 1e8
+typedef int ElementType;
+typedef struct Node *PtrToNode;
+struct Node { /* 队列中的结点 */
+    ElementType Data;
+    PtrToNode Next;
+};
+typedef PtrToNode Position;
 
-int main()
+struct QNode {
+    Position Front, Rear;  /* 队列的头、尾指针 */
+    int MaxSize;           /* 队列最大容量 */
+};
+typedef struct QNode *Queue;
+
+bool IsEmpty( Queue Q )
 {
-    int *p;
-    int i;
-    p=&i;
-    *p=100;
-    printf("p=%x\n",p);
-    printf("&p=%x\n",&p);
-    printf("*p=%x\n",*p);
-    printf("i=%x\n",i);
-    printf("&i=%x\n",&i);
+    return ( Q->Front == NULL);
+}
+
+ElementType DeleteQ( Queue Q )
+{
+    Position FrontCell;
+    ElementType FrontElem;
+    
+    if  ( IsEmpty(Q) ) {
+        printf("队列空");
+        return ERROR;
+    }
+    else {
+        FrontCell = Q->Front;
+        if ( Q->Front == Q->Rear ) /* 若队列只有一个元素 */
+            Q->Front = Q->Rear = NULL; /* 删除后队列置为空 */
+        else
+            Q->Front = Q->Front->Next;
+        FrontElem = FrontCell->Data;
+        
+        free( FrontCell );  /* 释放被删除结点空间  */
+        return  FrontElem;
+    }
 }
